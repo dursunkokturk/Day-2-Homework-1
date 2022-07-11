b2e6aaf1c57261ef9e15974aed07f9d64a9501af
Dursun Köktürk <dursunserefkokturk@gmail.com>
2022-07-11 17:50:28 +0300

    Day 2 Homework 1

---
+++ src/homework_20/Menus.java
@@ -0,0 +1,16 @@
+package homework_20;
+
+public class Menus {
+	
+	public Menus() {
+		System.out.println("Menus");
+	}
+	
+	public Menus (int id,String name) {
+		this();
+		this.id = id;
+		this.name = name;
+	}
+	int id;
+	String name;
+}
